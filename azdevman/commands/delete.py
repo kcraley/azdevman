31b7ca42e6ba39d601fbe8cdc07831c6d28f181e
Keith Craley <kcral21@gmail.com>
2019-03-10 16:34:11 -0400

    add basic delete repository with confirmation

--- azdevman/commands/delete.py
+++ azdevman/commands/delete.py
@@ -1,8 +1,27 @@
 import click
-from azdevman.utils.context import pass_context
+
 
 @click.group('delete')
-@pass_context
-def delete():
+@click.pass_obj
+def delete(ctx):
     """Delete Azure DevOps resources"""
-    pass
+
+
+@delete.command('repo')
+@click.option('-p', '--project', 'project',
+              help='Project name or id to create the repository in')
+@click.argument('repository_names', nargs=-1, required=True)
+@click.pass_obj
+def create_repo(ctx, project, repository_names):
+    """Delete an Azure DevOps repository"""
+    try:
+        click.confirm('Are you sure you want to delete these repositories?',
+                      default=False, abort=True)
+        _git_client = ctx.connection.clients.get_git_client()
+        if not project:
+            project = ctx._azure_devops_project
+        for repo_name in repository_names:
+            repository = _git_client.get_repository(repo_name, project)
+            _git_client.delete_repository(repository.id, repository.project.name)
+    except Exception as err:
+        raise click.UsageError(err)
