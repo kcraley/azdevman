b7d3b06d49f1586463df836a0bf5019d39154ea3
Keith Craley <kcral21@gmail.com>
2019-03-10 16:04:08 -0400

    add basic create repository command

--- azdevman/commands/create.py
+++ azdevman/commands/create.py
@@ -1,8 +1,31 @@
 import click
-from azdevman.utils.context import pass_context
+
 
 @click.group('create')
-@pass_context
-def create():
+@click.pass_obj
+def create(ctx):
     """Create Azure DevOps resources"""
-    pass
+
+
+@create.command('repo')
+@click.option('-p', '--project', 'project',
+              help='Project name or id to create the repository in')
+@click.argument('repository_names', nargs=-1, required=True)
+@click.pass_obj
+def create_repo(ctx, project, repository_names):
+    """Create an Azure DevOps repository"""
+    try:
+        _git_client = ctx.connection.clients.get_git_client()
+        if not project:
+            project = ctx._azure_devops_project
+        for repo_name in repository_names:
+            _git_create_options = ctx.models.git_models.GitRepositoryCreateOptions(repo_name)
+            _git_client.create_repository(_git_create_options, project)
+    except Exception as err:
+        raise click.UsageError(err)
+
+
+@create.command('build-definition')
+@click.pass_obj
+def create_build_definition(ctx):
+    """Create an Azure DevOps build definition"""
