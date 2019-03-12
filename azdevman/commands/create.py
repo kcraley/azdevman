5942e74f5158b5cacc8ecb07e9124f239537a53c
Keith Craley <kcral21@gmail.com>
2019-03-11 21:13:27 -0400

    add create build definition functionality

--- azdevman/commands/create.py
+++ azdevman/commands/create.py
@@ -1,4 +1,8 @@
 import click
+from azdevman.utils._const import (
+    AZ_DEFAULT_BUILD_DEF_PROCESS,
+    AZ_DEFAULT_BUILD_DEF_QUEUE
+)
 
 
 @click.group('create')
@@ -27,6 +31,64 @@ def create_repo(ctx, project, repository_names):
 
 
 @create.command('build-definition')
+@click.option('-f', '--copy-from', 'copy_from',
+              help='Build definition to copy from')
+@click.option('-r', '--repo', 'repo_name', required=True,
+              help='The repository to build from')
+@click.option('-p', '--project', 'project',
+              help='Project name or id to create the build-definition in')
+@click.argument('definition_name', nargs=1, required=True)
 @click.pass_obj
-def create_build_definition(ctx):
+def create_build_definition(ctx, definition_name, repo_name, copy_from, project):
     """Create an Azure DevOps build definition"""
+    try:
+        _git_client = ctx.connection.clients.get_git_client()
+        _build_client = ctx.connection.clients.get_build_client()
+        if not project:
+            project = ctx._azure_devops_project
+        try:
+            # Get repository information to build BuildRepository model
+            _repo = _git_client.get_repository(repo_name, project)
+            _build_repo_model = ctx.models.build_models.BuildRepository(
+                default_branch=_repo.default_branch,
+                id=_repo.id,
+                name=_repo.name,
+                url=_repo.remote_url,
+                type='TfsGit'
+            )
+
+            # Set default values
+            _build_process = AZ_DEFAULT_BUILD_DEF_PROCESS
+            _build_queue = AZ_DEFAULT_BUILD_DEF_QUEUE
+
+            # Create base BuildDefinition model from defaults
+            _build_def_model = ctx.models.build_models.BuildDefinition(
+                name=definition_name,
+                process=_build_process,
+                repository=_build_repo_model,
+                queue=_build_queue
+            )
+
+            if copy_from:
+                # Get BuildDefinition properties from copied location
+                _build_def = _build_client.get_definitions(project, copy_from)
+                _build_def = _build_client.get_definition(project, _build_def[0].id)
+                _build_process = _build_def.process
+                _build_options_model = _build_def.options
+                _build_queue_models = _build_def.queue
+                _build_retention_model = _build_def.retention_rules
+                _build_triggers = _build_def.triggers
+
+                _build_def_model.process = _build_process
+                _build_def_model.options = _build_options_model
+                _build_def_model.queue = _build_queue_models
+                _build_def_model.retention_rules = _build_retention_model
+                _build_def_model.triggers = _build_triggers
+
+            _new_definition = _build_client.create_definition(_build_def_model, project)
+            click.echo('Created new definition: ' + _new_definition.name)
+            click.echo('Link: ' + _new_definition._links.additional_properties.get('editor').get('href'))
+        except Exception as err:
+            raise click.UsageError(err)
+    except Exception as err:
+        raise click.UsageError(err)
--- azdevman/utils/_const.py
+++ azdevman/utils/_const.py
@@ -5,13 +5,13 @@ This module contains constant variables that will not change
 """
 
 # Environment Variables
-AZDEVMAN_ENV_PREFIX = 'AZDEVMAN_'
+AZDEVMAN_ENV_PREFIX = "AZDEVMAN_"
 
 # Azure Devops
-AZ_BASE_URL = 'https://dev.azure.com/'
-AZ_DEFAULT_ORG = 'ORGANIZATION'
-AZ_DEFAULT_PAT = 'UEFUCg=='
-AZ_DEFAULT_PROJECT = 'PROJECT'
+AZ_BASE_URL = "https://dev.azure.com/"
+AZ_DEFAULT_ORG = "ORGANIZATION"
+AZ_DEFAULT_PAT = "UEFUCg=="
+AZ_DEFAULT_PROJECT = "PROJECT"
 
 # Config file
 CONFIG_DIR = ".azdevman"
@@ -25,5 +25,34 @@ CONFIG_FILE_DEFAULT_CONTENT = {
             "Personal Access Token": AZ_DEFAULT_PAT,
             "Project": AZ_DEFAULT_PROJECT
         }
+    }
+}
+
+# Azure DevOps build definition
+AZ_DEFAULT_BUILD_DEF_PROCESS = {
+    "phases": [
+        {
+            "condition": "succeeded()",
+            "jobAuthorizationScope": "projectCollection",
+            "name": "Agent job 1",
+            "refName": "Job_1",
+            "target": {
+                "allowScriptsAuthAccessOption": False,
+                "executionOptions": {
+                    "type": 0
+                },
+                "type": 1
+            }
+        }
+    ],
+    "type": 1
+}
+AZ_DEFAULT_BUILD_DEF_QUEUE = {
+    "id": 12,
+    "name": "Hosted VS2017",
+    "pool": {
+        "id": 3,
+        "is_hosted": True,
+        "name": "Hosted VS2017"
     }
 }
