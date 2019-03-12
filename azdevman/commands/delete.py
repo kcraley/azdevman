b46e7e4c07171853bed8a4e6d0a09f7fbd1ad4f6
Keith Craley <kcral21@gmail.com>
2019-03-11 21:13:46 -0400

    add delete build definition functionality

--- azdevman/commands/delete.py
+++ azdevman/commands/delete.py
@@ -9,7 +9,7 @@ def delete(ctx):
 
 @delete.command('repo')
 @click.option('-p', '--project', 'project',
-              help='Project name or id to create the repository in')
+              help='Project name or id the repository in')
 @click.argument('repository_names', nargs=-1, required=True)
 @click.pass_obj
 def create_repo(ctx, project, repository_names):
@@ -26,3 +26,23 @@ def create_repo(ctx, project, repository_names):
             click.echo('Deleted repository ' + repo_name + ' within project ' + project)
     except Exception as err:
         raise click.UsageError(err)
+
+
+@delete.command('build-definition')
+@click.option('-p', '--project', 'project',
+              help='Project name or id the build definition in')
+@click.argument('build_definitions', nargs=-1, required=True)
+@click.pass_obj
+def delete_build_definition(ctx, project, build_definitions):
+    """Delete an Azure DevOps build definition"""
+    try:
+        click.confirm('Are you sure you want to delete these build definitions?',
+                      default=False, abort=True)
+        _build_client = ctx.connection.clients.get_build_client()
+        if not project:
+            project = ctx._azure_devops_project
+        for build_definition in build_definitions:
+            definition = _build_client.get_definitions(project, build_definition)
+            _build_client.delete_definition(project, definition[0].id)
+    except Exception as err:
+        raise err
